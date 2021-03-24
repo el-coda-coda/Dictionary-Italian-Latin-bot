b68d2c147c46053743f50709148682f8560a4262
el-coda-coda <66428797+el-coda-coda@users.noreply.github.com>
2021-03-24 19:33:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const bot_token = "ODIzOTQ1ODM3NjIwNjI1NDI4.YFoNWw.kBENN8jcAKUxDyxMCnoGQyCZI_o"
+
+console.log("Dicitionary-bot is started")
+console.log("--------------------------")
+console.log("")
+
+
+client.on("message", (message) => {
+    if (message.content.startsWith("!trad")){
+        let script = String("https:" + "//www.dizionario-latino.com/dizionario-latino-italiano.php?parola=" + String(message.content.substr(6)))
+        message.channel.send(script).then(msg => msg.delete({timeout: 150000}))
+    }
+    if (message.content.startsWith("!dec")){
+        let script = String("https:" + "//www.dizionario-latino.com/dizionario-latino-flessione.php?parola=" + String(message.content.substr(5)))
+        message.channel.send(script).then(msg => msg.delete({timeout: 150000}))
+    }
+})
+
+client.login(process.env.bot_token)
